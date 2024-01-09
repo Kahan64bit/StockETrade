aab4caf7af78bd92ed9d20c3bed7882bcf903fcb
Kahan Patel <106117072+Kahan64bit@users.noreply.github.com>
2024-01-08 22:33:11 -0500

    Implemented Langchain

--- ai.py
+++ ai.py
@@ -1,18 +1,23 @@
-from datetime import date
-import openai
+import os
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import Chroma
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
 from api.config import api_key_OPENAI
 
-openai.api_key = api_key_OPENAI
+os.environ["OPENAI_API_KEY"] = api_key_OPENAI
+model_id = 'gpt-3.5-turbo'
 
+def get_stock_suggestion(query):
+    loader = TextLoader("data.txt")
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
 
-today = date.today()
-def generate_stock_prediction(ticker):
-    response = openai.Completion.create(
-        prompt=f"Is {ticker} a good buy for {today}", 
-        model="gpt-3.5-turbo", 
-        max_tokens=1000, 
-        temperature=1,
-        n=1,stop=['---'])
-    for result in response:
-        print(result.text)
-    
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(texts, embeddings)
+
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}))
+    print(qa.run(query))
